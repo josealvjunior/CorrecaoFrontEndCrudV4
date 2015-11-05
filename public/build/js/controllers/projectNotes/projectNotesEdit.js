b77a2d3a183f7d7bfa4ed03bc9a846d544d84a52
Juninho <morbios@gmail.com>
2015-11-04 23:40:44 -0200

    Correção edit v4

---
+++ public/build/js/controllers/projectNotes/projectNotesEdit.js
@@ -0,0 +1,16 @@
+angular.module('app.controllers')
+    .controller('ProjectNotesEditController',
+    ['$scope','$location','$routeParams','ProjectNotes',
+        function($scope,$location,$routeParams, ProjectNotes){
+        $scope.projectNotes = ProjectNotes.get({
+            id: $routeParams.id,
+            noteId: $routeParams.noteId
+        });
+        $scope.save = function () {
+            if($scope.form.$valid){
+                ProjectNotes.update({id: $scope.projectNotes.id, noteId: $scope.projectNotes.noteId},$scope.projectNotes,function(){
+                    $location.path('/projects/' + $routeParams.id +'/notes');
+                });
+            }
+        }
+    }]);
